b2a65a3fff9d0ac116782d82f722d1e2f5d8440f
Ling <751167931@qq.com>
2023-10-09 20:37:22 +0800

    Update Neon City Scene

--- Assets/Scripts/Player/BasePlayer.cs
+++ Assets/Scripts/Player/BasePlayer.cs
@@ -6,6 +6,7 @@ public class BasePlayer : MonoBehaviour
 {
     [Header("组件")]
     public CharacterController _controller;
+    public Animator _animator;
 
     [Header("行为控制")]
     public bool isControllable = true;
@@ -20,9 +21,15 @@ public class BasePlayer : MonoBehaviour
     public float RotateSpeed;
     public float Health = 100;
 
+    // 检测是否落地
+    public float Gravity;
+    public bool isGround;
+    public float CheckRadius;
+    public LayerMask GroundMask;
+
     [Header("计算属性")]    
-    public Vector3 CurVelocity = Vector3.zero;  // 当前速度
-    public Vector3 CurDirection = Vector3.zero; // 当前方向
+    [SerializeField]private Vector3 CurVelocity = Vector3.zero;  // 当前速度
+    [SerializeField]private Vector3 CurDirection = Vector3.zero; // 当前方向
 
 
     private void Awake()
@@ -38,14 +45,22 @@ public class BasePlayer : MonoBehaviour
     void Update()
     {
         Move();
+        CheckGround();
         Test();
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            _animator.SetBool("isRun", true);
+        }
+        else
+        {
+            _animator.SetBool("isRun", false);
+        }
     }
 
     public virtual void Move()
     {        
         // 获得当前输入的方向
-        CurDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        //float y = Camera.main.transform.rotation.eulerAngles.y;
+        CurDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));             
         // 获得当前相机的方向
         float y = CameraManager.Instance.CurrentMainCamera.rotation.eulerAngles.y;
         // 使当前角色方向围绕相机y轴旋转y度，使得转向一致
@@ -63,6 +78,17 @@ public class BasePlayer : MonoBehaviour
             Health -= 10;
             EventCenter.Instance.EventTrigger("PlayerCheckHealth", Health);            
         }
+    }
+
+    public void CheckGround()
+    {
+        isGround = Physics.CheckSphere(transform.position, CheckRadius, GroundMask);    
+        if(isGround && CurVelocity.y < 0)
+        {
+            CurVelocity.y = -2;
+        }
+        CurVelocity.y -= Time.deltaTime * Gravity;
+        _controller.Move(CurVelocity);
     }
 
     private void OnDrawGizmos()
